3bd90b813dab2684ebce0645995bbfbce776a546
aubin.fraccaro <aubin.f@lycee-de-beauvoir.education>
2024-12-18 12:30:46 +0100

    Projet presque acheve Menu Presque complete Gestion des erreurs presque terminees : A finir pour UnContreBotFacile
    Ajout de UnContreBotFacile :
    Dans cette difficulté il ne peux pas y avoir plus d'une fois la meme couleur

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cctype>
 using namespace std;
 
 void clearScreen() {
@@ -9,8 +10,7 @@ void clearScreen() {
     }
 }
 
-void uncontreun(){
-    char couleurs [4] = {'R','V','B','J'};
+void uncontreun() {
     char tabJoueur [4] = {};
     char tabOrdi [4] = {};
     int nbTentatives = 10;
@@ -19,13 +19,12 @@ void uncontreun(){
     for (int i = 0; i < size(tabOrdi); i++) {
         cout <<"Saisissez la couleur position numero "<< i+1 << "/4 a faire deviner \n(R)ouge  (V)ert  (B)leu  (J)aune en majuscule\n:   ";
         cin >> tabOrdi [i];
+        tabOrdi[i] = static_cast<char>(toupper(tabOrdi[i]));
         if (tabOrdi[i] != 'R' && tabOrdi[i] != 'V' && tabOrdi[i] != 'B' && tabOrdi[i] != 'J') {
-            cout << "Veuillez saisir (R)ouge ou (V)ert ou (B)leu ou (J)aune en majuscule";
-            break;
+            cout << "Veuillez saisir (R)ouge ou (V)ert ou (B)leu ou (J)aune en majuscule\n";
+            i--;
         }
-        else continue;
     }
-    int compteur = 0;
     int tour = 1;
     cout << "\n   ---Debut de la partie---";
     clearScreen();
@@ -34,8 +33,13 @@ void uncontreun(){
         cout << "Vous avez : " << nbPoints << "points\n\n";
         int compteur = 0;
         for (int j = 0; j < 4; j++) {
-            cout <<"Saisissez votre tentative de devinette pour la position : "<< j+1<<" : \n";
+            cout <<"Saisissez votre tentative de devinette pour la position : "<< j+1<<"/4 : \n";
             cin >> tabJoueur [j];
+            tabJoueur [j] = static_cast<char>((tabJoueur [j]));
+            if (tabJoueur[j] != 'R' && tabJoueur[j] != 'V' && tabJoueur[j] != 'B' && tabJoueur[j] != 'J'){
+                cout << "Veuillez saisir (R)ouge ou (V)ert ou (B)leu ou (J)aune en majuscule\n";
+                j--;
+            }
         }
         for (int j = 0; j < 4; j++) {
             if (tabJoueur[j] == tabOrdi[j]) {
@@ -47,11 +51,19 @@ void uncontreun(){
         tour++;
         if(compteur != 4) {
             clearScreen();
-            cout << "\n\n   ---Tour Suivant---" <<endl;
             nbPoints -= 1000;
+            if (nbPoints <= 0) {
+                cout << "\nVous n'avez plus de points. Partie terminee !\n La solution etait : ";
+                for (char c : tabOrdi) {
+                    cout << c;
+                }
+                break;
+            }
+            clearScreen();
+            cout << "\n\n   ---Tour Suivant---" <<endl;
             continue;
         }
-        else if (compteur == 4) {
+        else {
             cout << "Vous avez : " << nbPoints << "points\n";
             break;
         }
@@ -67,7 +79,6 @@ void uncontrebot() {
     }
     int nbTentatives = 10;
     int nbPoints = 10000;
-    int compteur = 0;
     int tour = 1;
     cout << "\n   ---Debut de la partie---";
     for (int i = 0; i < nbTentatives; i++) {
@@ -75,8 +86,82 @@ void uncontrebot() {
         cout << "Points : " << nbPoints<<"\n\n";
         int compteur = 0;
         for (int j = 0; j < 4; j++) {
-            cout <<"Saisissez votre tentative de devinette pour la position : "<< j+1<<" \n:   ";
+            cout <<"Saisissez votre tentative de devinette pour la position : "<< j+1<<"/4 \n(R)ouge  (V)ert  (B)leu  (J)aune en majuscule\n:   ";
             cin >> tabJoueur [j];
+            if (toupper(tabJoueur[j]) != 'R' && toupper(tabJoueur[j]) != 'V' && toupper(tabJoueur[j]) != 'B' && toupper(tabJoueur[j]) != 'J'){
+                cout << "Veuillez saisir (R)ouge ou (V)ert ou (B)leu ou (J)aune en majuscule\n";
+                j--;
+            }
+            tabJoueur [j] = static_cast<char>((tabJoueur [j]));
+        }
+        for (int j = 0; j < 4; j++) {
+            if (tabJoueur[j] == tabOrdi[j]) {
+                compteur++;
+            }
+
+        }
+        cout << "Vous avez "<< compteur<<" guess corrects ";
+        tour++;
+        if(compteur != 4) {
+            cout << "\n\n   ---Tour Suivant---" <<endl;
+            nbPoints -= 1000;
+            if (nbPoints <= 0) {
+                cout << "\nVous n'avez plus de points. Partie terminee !\n La solution etait : ";
+                for (char c : tabOrdi) {
+                    cout << c;
+                }
+                continue;
+            }
+        }
+        else{
+            cout << "Vous avez : " << nbPoints << "points\n";
+            break;
+        }
+    }
+}
+
+void UnContreBotFacile() {
+    char couleurs [4] = {'R','V','B','J'};
+    char tabJoueur [4] = {};
+    char tabOrdi [4] = {};
+    // Remplir tabOrdi avec des couleurs uniques
+    for (int i = 0; i < 4; i++) {
+        char couleurGenerée;
+        bool couleurExistante;
+
+        do {
+            couleurGenerée = couleurs[rand() % 4]; // Choisir une couleur aléatoire
+            couleurExistante = false;
+
+            // Vérifier si la couleur existe déjà dans tabOrdi
+            for (int j = 0; j < i; j++) {
+                if (tabOrdi[j] == couleurGenerée) {
+                    couleurExistante = true;
+                    break;
+                }
+            }
+
+        } while (couleurExistante); // Tant que la couleur est déjà dans tabOrdi, on recommence
+
+        tabOrdi[i] = couleurGenerée; // Ajouter la couleur générée unique dans tabOrdi
+    }
+    cout << tabOrdi;
+    int nbTentatives = 10;
+    int nbPoints = 10000;
+    int tour = 1;
+    cout << "\n   ---Debut de la partie---";
+    for (int i = 0; i < nbTentatives; i++) {
+        cout << "\n\nTour numero : "<< tour<<endl;
+        cout << "Points : " << nbPoints<<"\n\n";
+        int compteur = 0;
+        for (int j = 0; j < 4; j++) {
+            cout <<"Saisissez votre tentative de devinette pour la position : "<< j+1<<"/4 : \n(R)ouge  (V)ert  (B)leu  (J)aune\n:   ";
+            cin >> tabJoueur [j];
+            if (tabJoueur[j] != 'R' && tabJoueur[j] != 'V' && tabJoueur[j] != 'B' && tabJoueur[j] != 'J'){
+                cout << "\nVeuillez saisir (R)ouge ou (V)ert ou (B)leu ou (J)aune en majuscule\n";
+                j--;
+            }
+                tabJoueur [j] = static_cast<char>((tabJoueur [j]));
         }
         for (int j = 0; j < 4; j++) {
             if (tabJoueur[j] == tabOrdi[j]) {
@@ -91,31 +176,37 @@ void uncontrebot() {
             nbPoints -= 1000;
             continue;
         }
-        else if (compteur == 4) {
+        else{
             cout << "Vous avez : " << nbPoints << "points\n";
             break;
         }
     }
 }
 
-int main() {
-    srand(time(0));
-    int choix = 0;
-    cout << "\n\n     ---Menu Principal MasterMind---   \n\n 1- 1vs1\n 2- 1vsBot\n\nSaisie : ";
-    cin >> choix;
-    if (choix == 1) {
-        uncontreun();
-        cout << endl << endl << endl;
 
+int main() {
+        srand(time(nullptr));
+        int choix = 0;
+        cout << "\n\n     ---Menu Principal MasterMind---   \n\n 1- 1vs1\n 2- 1vsBot(Facile)\n 3- 1vsBot(100%aleatoire)\n\nSaisie : ";
+        cin >> choix;
+        if (choix == 1) {
+            uncontreun();
+            cout << endl << endl << endl;
+
+        }
+        else if (choix == 2) {
+            UnContreBotFacile();
+            cout << endl << endl << endl;
+        }
+        else if (choix == 3) {
+            uncontrebot();
+            cout << endl << endl << endl;
+        }
+        else {
+            cout << "Veuillez saisir une valeur comprise dans le menu";
+        }
+        return 0;
     }
-    else if (choix == 2) {
-        uncontrebot();
-        cout << endl << endl << endl;
-    }
-    else {
-        cout << "Veuillez saisir une valeur comprise dans le menu";
-    }
-    return 0;
-}
+
 
 
